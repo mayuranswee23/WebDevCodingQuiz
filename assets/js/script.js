bc978935a1e8b4daa78c42a1402f8cc0669b0b9c
Mayuran <mayuranswee23@gmail.com>
2021-05-16 01:02:48 -0400

    add js questions

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+var codeQuiz = document.getElementById("startbutton");
+
+//Quiz Questions
+var quizQuestions = [
+    {
+    question: "1+1?",
+    options: ["1","2","3","4"],
+    answer: "2"
+},
+{
+    question: "2+1?",
+    options: ["1","2","3","4"],
+    answer: "3"
+},
+{
+    question: "1+3?",
+    options: ["1","2","3","4"],
+    answer: "4"
+},
+{
+    question: "2+2?",
+    options: ["1","2","3","4"],
+    answer: "4"
+},
+];
